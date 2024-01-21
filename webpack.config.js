1b3c7839502b407f62340b11ed75c747bc3f758a
stokato <demyanovpaul@yandex.ru>
2024-01-21 11:30:13 +0300

    Бонусное задание

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+const path = require('path');
+
+module.exports = {
+  context: path.resolve(__dirname, 'src'),
+  entry: {
+    main: './index.js',
+  },
+  output: {
+    path: path.resolve(__dirname, './dist/js'),
+    filename: '[name].js',
+  },
+  devServer: {
+    hot: true,
+    static: {
+      directory: path.join(__dirname, 'dist'),
+      watch: true,
+    },
+  },
+};
